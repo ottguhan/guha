f23eac00c4d8d720ecdeeeda4df6312b2a97db18
ottguhan <86597680+ottguhan@users.noreply.github.com>
2021-06-28 16:17:28 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Query:</i> <Bold>{query}</code>
+    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Query:</i> <Code>{query}</code>
 Join :@ottrockersz "
         
     try:
